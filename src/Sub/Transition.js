f20134ec8b5916785ce5d2ea73b88c3f33aac64d
igorho2000 <igorho2000@gmail.com>
2022-08-25 18:58:44 +0800

    Final adjustments

--- src/Sub/Transition.js
+++ src/Sub/Transition.js
@@ -26,6 +26,7 @@ export function EndAnimation(props) {
             <div className="end-background" >
                 <h1 className='end-thank'>Thanks for watching.</h1>
                 <h2 className='end-credit'>Created by Igor Ho, August 2022.</h2>
+                <h2 className='end-credit'>(After 72hrs and 12 cups of coffee)</h2>
             </div>
         </div>
         

1bf8a3418e371351aba4cb5269a9eedadd0e8c30
Igor Ho <igorho2000@gmail.com>
2024-03-13 15:27:37 +0800

    Modified Control Buttons and Updated Resume

--- src/App.js
+++ src/App.js
@@ -11,15 +11,15 @@ import i18n from "i18next";
 import { initReactI18next, useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  defineDarkmode,
-  defineLanguage,
-  selectAnimation,
-  selectDarkmode,
-  selectLangEn,
-  toggleDarkmode,
-  toggleLanguage,
-  selectLoad,
-  finishLoad,
+	defineDarkmode,
+	defineLanguage,
+	selectAnimation,
+	selectDarkmode,
+	selectLangEn,
+	toggleDarkmode,
+	toggleLanguage,
+	selectLoad,
+	finishLoad,
 } from "./store/utilSlice";
 
 // Component Imports
@@ -42,121 +42,119 @@ import lang from "./static/lang/lang.json";
 import { useEffect } from "react";
 
 i18n.use(initReactI18next).init({
-  resources: lang,
-  lng: "en",
-  fallbackLng: "en",
+	resources: lang,
+	lng: "en",
+	fallbackLng: "en",
 
-  interpolation: {
-    escapeValue: false,
-  },
+	interpolation: {
+		escapeValue: false,
+	},
 });
 
 function App() {
-  const { i18n } = useTranslation();
-  const dispatch = useDispatch();
-  const langEn = useSelector(selectLangEn);
-  const darkmode = useSelector(selectDarkmode);
-  const animation = useSelector(selectAnimation);
-  const load = useSelector(selectLoad);
+	const { i18n } = useTranslation();
+	const dispatch = useDispatch();
+	const langEn = useSelector(selectLangEn);
+	const darkmode = useSelector(selectDarkmode);
+	const animation = useSelector(selectAnimation);
+	const load = useSelector(selectLoad);
 
-  useEffect(() => {
-    const lang = localStorage.getItem("lang");
-    const darkmode = localStorage.getItem("darkmode");
-    if (lang === undefined) {
-      localStorage.setItem("lang", "en");
-    } else {
-      dispatch(defineLanguage(lang));
-      i18n.changeLanguage(lang);
-    }
-    if (darkmode === undefined) {
-      localStorage.setItem("darkmode", "light");
-    } else if (darkmode === "dark") {
-      dispatch(defineDarkmode(true));
-    } else {
-      dispatch(defineDarkmode(false));
-    }
-    setTimeout(() => {
-      dispatch(finishLoad());
-    }, 1000);
-  }, []);
+	useEffect(() => {
+		const lang = localStorage.getItem("lang");
+		const darkmode = localStorage.getItem("darkmode");
+		if (lang === undefined) {
+			localStorage.setItem("lang", "en");
+		} else {
+			dispatch(defineLanguage(lang));
+			i18n.changeLanguage(lang);
+		}
+		if (darkmode === undefined) {
+			localStorage.setItem("darkmode", "light");
+		} else if (darkmode === "dark") {
+			dispatch(defineDarkmode(true));
+		} else {
+			dispatch(defineDarkmode(false));
+		}
+		setTimeout(() => {
+			dispatch(finishLoad());
+		}, 1000);
+	}, []);
 
-  const Blogpaths = BlogList.map((item, index) => (
-    <Route
-      path={`/blog/${item.type}${index}`}
-      element={<Posts info={item} />}
-    />
-  ));
+	const Blogpaths = BlogList.map((item, index) => (
+		<Route
+			path={`/blog/${item.type}${index}`}
+			element={<Posts info={item} />}
+		/>
+	));
 
-  const Portpaths = PortList.map((item, index) => (
-    <Route
-      path={`/portfolio/port${index}`}
-      element={<PortPost info={item} />}
-    />
-  ));
+	const Portpaths = PortList.map((item, index) => (
+		<Route
+			path={`/portfolio/port${index}`}
+			element={<PortPost info={item} />}
+		/>
+	));
 
-  return (
-    <>
-      <div className={`App ${darkmode && "d-background"}`}></div>
-      <div className="ge-control">
-        <button
-          className={`ge-lang ${darkmode && "d-border d-control"}`}
-          onClick={() => {
-            darkmode
-              ? localStorage.setItem("darkmode", "light")
-              : localStorage.setItem("darkmode", "dark");
-            dispatch(toggleDarkmode());
-          }}
-        >
-          <span class="material-icons-round">
-            {darkmode ? "light_mode" : "dark_mode"}
-          </span>
-        </button>
-        {langEn && (
-          <button
-            className={`ge-lang ${darkmode && "d-border d-control"}`}
-            onClick={() => {
-              i18n.changeLanguage("ch");
-              dispatch(toggleLanguage());
-              localStorage.setItem("lang", "ch");
-            }}
-          >
-            <span class="material-icons-round">translate</span>
-            中文
-          </button>
-        )}
-        {langEn === false && (
-          <button
-            className={`ge-lang ${darkmode && "d-border d-control"}`}
-            onClick={() => {
-              i18n.changeLanguage("en");
-              dispatch(toggleLanguage());
-              localStorage.setItem("lang", "en");
-            }}
-          >
-            <span class="material-icons-round">translate</span>
-            English
-          </button>
-        )}
-      </div>
-      <Routes>
-        {!load && (
-          <>
-            <Route path="/" element={!animation.inProgress && <Home />} />
-            <Route path="/navigation" element={<Navigation />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/certificate" element={<Certificate />} />
-            {Blogpaths}
-            {Portpaths}
-          </>
-        )}
-      </Routes>
-      <Load inProgress={load} />
-      <Transition inProgress={animation.inProgress} />
-      <AnimationEndScreen inProgress={animation.endAnimation} />
-    </>
-  );
+	return (
+		<>
+			<div className={`App ${darkmode && "d-background"}`}></div>
+			<div className="ge-control">
+				<button
+					className={`d-button ${darkmode && "d-control"}`}
+					onClick={() => {
+						darkmode
+							? localStorage.setItem("darkmode", "light")
+							: localStorage.setItem("darkmode", "dark");
+						dispatch(toggleDarkmode());
+					}}
+				>
+					<span class="material-icons-round">
+						{darkmode ? "light_mode" : "dark_mode"}
+					</span>
+				</button>
+				{langEn && (
+					<button
+						className={`d-button ${darkmode && "d-control"}`}
+						onClick={() => {
+							i18n.changeLanguage("ch");
+							dispatch(toggleLanguage());
+							localStorage.setItem("lang", "ch");
+						}}
+					>
+						<span class="material-icons-round">translate</span>
+					</button>
+				)}
+				{langEn === false && (
+					<button
+						className={`d-button ${darkmode && "d-border d-control"}`}
+						onClick={() => {
+							i18n.changeLanguage("en");
+							dispatch(toggleLanguage());
+							localStorage.setItem("lang", "en");
+						}}
+					>
+						<span class="material-icons-round">translate</span>
+					</button>
+				)}
+			</div>
+			<Routes>
+				{!load && (
+					<>
+						<Route path="/" element={!animation.inProgress && <Home />} />
+						<Route path="/navigation" element={<Navigation />} />
+						<Route path="/about" element={<About />} />
+						<Route path="/portfolio" element={<Portfolio />} />
+						<Route path="/blog" element={<Blog />} />
+						<Route path="/certificate" element={<Certificate />} />
+						{Blogpaths}
+						{Portpaths}
+					</>
+				)}
+			</Routes>
+			<Load inProgress={load} />
+			<Transition inProgress={animation.inProgress} />
+			<AnimationEndScreen inProgress={animation.endAnimation} />
+		</>
+	);
 }
 
 export default App;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,113 +3,86 @@ import { useSelector } from "react-redux";
 import { selectDarkmode } from "../store/utilSlice";
 
 export default function Sidebar(props) {
-  const { t } = useTranslation();
-  const darkmode = useSelector(selectDarkmode);
-  const d = new Date();
-  const year = d.getYear() + 1900;
+	const { t } = useTranslation();
+	const darkmode = useSelector(selectDarkmode);
+	const d = new Date();
+	const year = d.getYear() + 1900;
 
-  return (
-    <div className={darkmode ? "d-text" : ""}>
-      <div className="si-cont">
-        <h3 className="si-title">{t("sidebar.contact-me")}</h3>
-        <a className="si-contact" href="mailto: igorho2000@gmail.com">
-          <img
-            src={`${props.path}sidebar/icon-email${
-              darkmode ? "-dark" : ""
-            }.svg`}
-            className="si-contact__logo"
-            alt="email"
-          />
-          <p className={`si-contact__text ${darkmode && "d-text"}`}>
-            igorho2000@gmail.com
-          </p>
-        </a>
-        <a
-          className="si-contact"
-          href="https://github.com/igorho2000"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={`${props.path}sidebar/icon-github${
-              darkmode ? "-dark" : ""
-            }.svg`}
-            className="si-contact__logo"
-            alt="github"
-          />
-          <p className={`si-contact__text ${darkmode && "d-text"}`}>
-            github.com/igorho2000
-          </p>
-        </a>
-        <a
-          className="si-contact"
-          href="https://linkedin.com/in/igorho"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={`${props.path}sidebar/icon-linkedin${
-              darkmode ? "-dark" : ""
-            }.svg`}
-            className="si-contact__logo"
-            alt="linkedin"
-          />
-          <p className={`si-contact__text ${darkmode && "d-text"}`}>
-            linkedin.com/in/igorho
-          </p>
-        </a>
-      </div>
-      <div className="si-cont">
-        <h3 className="si-title">{t("sidebar.hire")}</h3>
-        <a
-          className="si-contact"
-          href="https://www.fiverr.com/igorho423"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={`${props.path}sidebar/icon-fiverr.svg`}
-            className="si-contact__logo"
-            style={{ width: "24px" }}
-            alt="fiverr"
-          />
-          <p className={`si-contact__text ${darkmode && "d-text"}`}>
-            https://www.fiverr.com/igorho423
-          </p>
-        </a>
-        <a className="si-contact" href="https://www.fiverr.com/share/qQxAKX">
-          <p
-            className="si-contact__text"
-            style={{ margin: "0", fontStyle: "italic", color: "gray" }}
-          >
-            {t("sidebar.hire-description")}
-          </p>
-        </a>
-      </div>
-      <div className="si-cont">
-        <h3 className="si-title">{t("sidebar.download")}</h3>
-        <a
-          className={`ge-lang ${darkmode && "d-control d-border"}`}
-          style={{ fontSize: "1rem" }}
-          href={`${props.path}sidebar/Igor_Resume_Eng.pdf`}
-          download
-        >
-          <span className="material-icons-round">file_download</span>
-          <p>{t("sidebar.download-button")}</p>
-        </a>
-      </div>
-      <div>
-        <img
-          src={`${props.path}logo-main${darkmode ? "-dark" : ""}.svg`}
-          className="si-logo"
-          alt="one premium logo"
-        />
-        <p className="si-copyright">© {year} Project One Premium</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className={darkmode ? "d-text" : ""}>
+			<div className="si-cont">
+				<h3 className="si-title">{t("sidebar.contact-me")}</h3>
+				<a className="si-contact" href="mailto: igorho2000@gmail.com">
+					<img
+						src={`${props.path}sidebar/icon-email${
+							darkmode ? "-dark" : ""
+						}.svg`}
+						className="si-contact__logo"
+						alt="email"
+					/>
+					<p className={`si-contact__text ${darkmode && "d-text"}`}>
+						igorho2000@gmail.com
+					</p>
+				</a>
+				<a
+					className="si-contact"
+					href="https://github.com/igorho2000"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<img
+						src={`${props.path}sidebar/icon-github${
+							darkmode ? "-dark" : ""
+						}.svg`}
+						className="si-contact__logo"
+						alt="github"
+					/>
+					<p className={`si-contact__text ${darkmode && "d-text"}`}>
+						github.com/igorho2000
+					</p>
+				</a>
+				<a
+					className="si-contact"
+					href="https://linkedin.com/in/igorho"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<img
+						src={`${props.path}sidebar/icon-linkedin${
+							darkmode ? "-dark" : ""
+						}.svg`}
+						className="si-contact__logo"
+						alt="linkedin"
+					/>
+					<p className={`si-contact__text ${darkmode && "d-text"}`}>
+						linkedin.com/in/igorho
+					</p>
+				</a>
+			</div>
+			<div className="si-cont">
+				<h3 className="si-title">{t("sidebar.download")}</h3>
+				<a
+					className={`ge-lang ${darkmode && "d-control d-border"}`}
+					style={{ fontSize: "1rem" }}
+					href={`${props.path}sidebar/Igor_Resume_Eng.pdf`}
+					download
+				>
+					<span className="material-icons-round">file_download</span>
+					<p>{t("sidebar.download-button")}</p>
+				</a>
+			</div>
+			<div>
+				<img
+					src={`${props.path}logo-main${darkmode ? "-dark" : ""}.svg`}
+					className="si-logo"
+					alt="one premium logo"
+				/>
+				<p className="si-copyright">© {year} Project One Premium</p>
+			</div>
+		</div>
+	);
 }
 
 Sidebar.defaultProps = {
-  path: "./",
+	path: "./",
 };
